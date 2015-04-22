cf816d16453a4979f7402e6db0411f49b5a99652
JCarder <JCarder@DEV-DSK-Jon.Main.Infrastructure.Access411.com>
2015-04-22 13:47:03 -0400

    Created Servlet to get data from database

---
+++ IST411Project/web/SourcePackages/DBServlet.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SourcePackages;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author JCarder
+ */
+@WebServlet(urlPatterns = {"/DBServlet"})
+public class DBServlet extends HttpServlet {
+
+    
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+throws ServletException, IOException  {
+
+    try {
+        
+        String productID;
+        productID = req.getParameter("IDnum");
+        getServletConfig().getServletContext().getRequestDispatcher(
+        "product.jsp").forward(req,res);
+
+    } catch (ServletException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+}
+ 
